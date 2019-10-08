8a411275610ef68d09b7ba142a449476c534d421
cfigueirasvalverde <cfigueirasvalverde@nautilus8.ad.danielcastelao.org>
2019-10-08 11:01:00 +0200

    ejercicio3

---
+++ Extras_1/Ejercicio3/src/ejercicio3/Ejercicio3.java
@@ -0,0 +1,19 @@
+package ejercicio3;
+
+/**
+ *
+ * @author cfigueirasvalverde
+ */
+
+//realiza un programa que lea un numero de tres cifras y amose por pantalla las cifras del numero
+
+public class Ejercicio3 {
+
+   
+    public static void main(String[] args) {
+        
+        
+        
+    }
+    
+}
